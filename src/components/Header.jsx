b8a79e10f8cc5a39e563136c638fd21b16b34f52
camiyoung <anji6978@naver.com>
2022-06-24 20:48:16 +0900

    header 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "./css/header.module.css";
 
 export default function Header() {
   return (
-    <header className="header">
-      <div className="">헤더</div>
-      <Link to="/">
-        <button>home</button>
+    <header className={styles.header}>
+      <Link to="/" className={styles.nav}>
+        <span>home</span>
       </Link>
-      <Link to="/login">
-        <button>login</button>
+      <Link to="/login" className={styles.nav}>
+        <span>login</span>
       </Link>
-      <Link to="/signup">
-        <button>signup</button>
+      <Link to="/signup" className={styles.nav}>
+        <span>signup</span>
       </Link>
-      <Link to="/mypage">
-        <button>mypage</button>
+      <Link to="/mypage" className={styles.nav}>
+        <span>mypage</span>
       </Link>
     </header>
   );
