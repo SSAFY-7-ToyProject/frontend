9d813a97ed0a2d2e28811e62b1d2cd4e5ce84fe3
camiyoung <anji6978@naver.com>
2022-07-07 19:58:41 +0900

    내 일기만 보는 페이지 추가

---
+++ src/pages/MyDiary.jsx
@@ -0,0 +1,6 @@
+import React, { useEffect } from "react";
+import PostList from "../components/post/PostList";
+
+export default function PostListPage({ userid, setAppClassName }) {
+  return <PostList userid={userid} setAppClassName={setAppClassName} />;
+}
