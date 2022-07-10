5d2da30587cb38db83dadeaa43af7f52fe32457a
camiyoung <anji6978@naver.com>
2022-07-10 17:13:03 +0900

    request qusrud

--- src/api/index.js
+++ src/api/index.js
@@ -2,9 +2,11 @@ import axios from "axios";
 
 function Instance() {
   const instance = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "http://52.78.200.184:8080/",
     headers: {
       "Content-type": "application/json",
+      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+      "Access-Control-Allow-Credentials": true, // Required for cookies,
     },
   });
   return instance;
